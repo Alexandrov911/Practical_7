8618fdaae7c0520abdba02b3894621b27098e95e
Alexandrov911 <sashok17062002@mail.ru>
2021-11-04 17:05:10 +0700

    Практическая №7

---
+++ Практическая №7, второй курс/Практическая №7, второй курс/Form1.Designer.cs
@@ -0,0 +1,171 @@
+﻿
+namespace Практическая__7__второй_курс
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(111, 198);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(115, 20);
+            this.numericUpDown1.TabIndex = 1;
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4});
+            this.dataGridView1.Location = new System.Drawing.Point(111, 26);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.Size = new System.Drawing.Size(501, 150);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "ФИО";
+            this.Column1.Name = "Column1";
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Школа";
+            this.Column2.Name = "Column2";
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Класс";
+            this.Column3.Name = "Column3";
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "Тест";
+            this.Column4.Name = "Column4";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(274, 191);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(115, 30);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Записать в файл";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(417, 191);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(115, 30);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Чтение из файла";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(637, 65);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Min";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(637, 113);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "Max";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Name = "Form1";
+            this.Text = "Min";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ Практическая №7, второй курс/Практическая №7, второй курс/Form1.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Практическая__7__второй_курс
+{
+    public partial class Form1 : Form
+    {
+        public struct Person
+        {
+            public string FIO;
+            public int school;
+            public int klass;
+            public int test;
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            dataGridView1.RowCount = Convert.ToInt32(numericUpDown1.Value);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                int n = Convert.ToInt32(numericUpDown1.Value);
+                Person[] mas = new Person[n];
+                for (int i = 0; i < n; i++)
+                {
+                    mas[i].FIO = dataGridView1[0, i].Value.ToString();
+                    mas[i].school = Convert.ToInt32(dataGridView1[1, i].Value);
+                    mas[i].klass = Convert.ToInt32(dataGridView1[2, i].Value);
+                    mas[i].test = Convert.ToInt32(dataGridView1[3, i].Value);
+                }
+                    StreamWriter sw = new StreamWriter(saveFileDialog1.FileName,
+                        true, System.Text.Encoding.Default);
+                    for (int i = 0; i < n; i++)
+                    {
+                        sw.WriteLine(mas[i].FIO);
+                        sw.WriteLine(mas[i].school);
+                        sw.WriteLine(mas[i].klass);
+                        sw.WriteLine(mas[i].test);
+                    }
+                    sw.Close();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                StreamReader sr = new StreamReader(openFileDialog1.FileName,
+                    System.Text.Encoding.Default);
+                Person[] mas = new Person[0];
+                int k = 0;
+                while (!sr.EndOfStream)
+                {
+                    k++;
+                    Array.Resize(ref mas, k);
+                    mas[k - 1].FIO = sr.ReadLine();
+                    mas[k - 1].school = Convert.ToInt32(sr.ReadLine());
+                    mas[k - 1].klass = Convert.ToInt32(sr.ReadLine());
+                    mas[k - 1].test = Convert.ToInt32(sr.ReadLine());
+                }
+                sr.Close();
+                dataGridView1.RowCount = k;
+                numericUpDown1.Value = k;
+                for (int i = 0; i < k; i++)
+                {
+                    dataGridView1[0, i].Value = mas[i].FIO;
+                    dataGridView1[1, i].Value = mas[i].school;
+                    dataGridView1[2, i].Value = mas[i].klass;
+                    dataGridView1[3, i].Value = mas[i].test;
+                }
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int n = dataGridView1.RowCount = Convert.ToInt32(numericUpDown1.Value);
+            Person[] mas = new Person[n];
+            for (int i = 0; i < n; i++)
+            {
+                mas[i].FIO = dataGridView1[0, i].Value.ToString();
+                mas[i].school = Convert.ToInt32(dataGridView1[1, i].Value);
+                mas[i].klass = Convert.ToInt32(dataGridView1[2, i].Value);
+                mas[i].test = Convert.ToInt32(dataGridView1[3, i].Value);
+            }
+            int min = mas[0].klass;
+            for(int i=0; i<n; i++)
+            {
+                if (min > mas[i].klass)
+                {
+                    min = mas[i].klass;
+                }
+                
+            }
+            MessageBox.Show(min.ToString());
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            int n = dataGridView1.RowCount = Convert.ToInt32(numericUpDown1.Value);
+            Person[] mas = new Person[n];
+            for (int i = 0; i < n; i++)
+            {
+                mas[i].FIO = dataGridView1[0, i].Value.ToString();
+                mas[i].school = Convert.ToInt32(dataGridView1[1, i].Value);
+                mas[i].klass = Convert.ToInt32(dataGridView1[2, i].Value);
+                mas[i].test = Convert.ToInt32(dataGridView1[3, i].Value);
+            }
+            int max = mas[0].klass;
+            for (int i = 0; i < n; i++)
+            {
+                if (max < mas[i].klass)
+                {
+                    max = mas[i].klass;
+                }
+
+            }
+            MessageBox.Show(max.ToString());
+        }
+    }
+}
